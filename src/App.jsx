6286be916824a80e44b2acfe9d6896a94c5a68a6
Валерій Зінченко <zinchenkomac@icloud.com>
2025-09-14 16:08:19 +0200

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
+import { useState, useEffect } from 'react';
+import { initialTodos, createTodo } from './todos.js';
 
-import './App.css'
-import Card from './Card'
-import { useCartContext } from "./context";
+export default function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [showActive, setShowActive] = useState(false);
+  const [activeTodos, setActiveTodos] = useState([]);
+  const [visibleTodos, setVisibleTodos] = useState([]);
+  const [footer, setFooter] = useState(null);
 
-function App() {
-  const {cards} = useCartContext()
+  useEffect(() => {
+    setActiveTodos(todos.filter(todo => !todo.completed));
+  }, [todos]);
+
+  useEffect(() => {
+    setVisibleTodos(showActive ? activeTodos : todos);
+  }, [showActive, todos, activeTodos]);
+
+  useEffect(() => {
+    setFooter(
+      <footer>
+        {activeTodos.length} todos left
+      </footer>
+    );
+  }, [activeTodos]);
 
   return (
     <>
-      <div>
-        {
-          cards.map((product)=><Card key={product.name} card={product}/>)
-        }
-      </div>
+      <label>
+        <input
+          type="checkbox"
+          checked={showActive}
+          onChange={e => setShowActive(e.target.checked)}
+        />
+        Show only active todos
+      </label>
+      <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])} />
+      <ul>
+        {visibleTodos.map(todo => (
+          <li key={todo.id}>
+            {todo.completed ? <s>{todo.text}</s> : todo.text}
+          </li>
+        ))}
+      </ul>
+      {footer}
     </>
-  )
+  );
 }
 
-export default App
+function NewTodo({ onAdd }) {
+  const [text, setText] = useState('');
+
+  function handleAddClick() {
+    setText('');
+    onAdd(createTodo(text));
+  }
+
+  return (
+    <>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={handleAddClick}>
+        Add
+      </button>
+    </>
+  );
+}
