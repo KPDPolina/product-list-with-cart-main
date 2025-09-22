517ed7c3330a27df856634baa27450973c3d7569
Polina Krasnova <krasnovapolina7@gmail.com>
2025-09-22 16:22:38 +0200

    make task_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { initialTodos, createTodo, getVisibleTodos } from './todos.js';
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
   const [text, setText] = useState('');
-  const [visibleTodos, setVisibleTodos] = useState([]);
+  // const [visibleTodos, setVisibleTodos] = useState([]);
 
-  useEffect(() => {
-    setVisibleTodos(getVisibleTodos(todos, showActive));
-  }, [todos, showActive]);
+  // useEffect(() => {
+  //   setVisibleTodos(getVisibleTodos(todos, showActive));
+  // }, [todos, showActive]);
+
+  const visibleTodos = useMemo(
+    () => getVisibleTodos(todos, showActive), [todos, showActive]
+  )
 
   function handleAddClick() {
     setText('');
     setTodos([...todos, createTodo(text)]);
   }
 
+  console.log("render");
+  
   return (
     <>
       <label>
